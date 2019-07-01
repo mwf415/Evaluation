764d782f46e3daff02d9553ff05180e2719915d2
mwf415 <mwf415@126.com>
2019-07-01 15:09:23 +0800

    初始化提交

---
+++ src/main/java/cn/onlov/evaluate/core/dao/mapper/CycleRolePermissionMapper.java
@@ -0,0 +1,16 @@
+package cn.onlov.evaluate.core.dao.mapper;
+
+import cn.onlov.evaluate.core.dao.entities.CycleRolePermission;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author kaifa
+ * @since 2019-01-04
+ */
+public interface CycleRolePermissionMapper extends BaseMapper<CycleRolePermission> {
+
+}
